a4cbc35a89c61b899219fed4c70b547f1a7cdb51
Tom Hooijenga <tomhooijenga@gmail.com>
2020-07-01 18:14:46 +0200

    Add zoom links

--- resize.js
+++ resize.js
@@ -6,13 +6,17 @@ const path = require('path');
 glob('./source/**/*.jpg', async (error, files) => {
   await Promise.all(files.map( async (file) => {
 
-    const destination = file.replace('./source', './img').replace('.jpg', '.webp');
+    const destination = file.replace('./source', './img').replace('.jpg', '');
     console.log(file, destination);
 
     await mkdirp(path.dirname(destination));
     await sharp(file)
       .resize({width: 1000})
       .webp()
-      .toFile(destination);
+      .toFile(destination + '.webp');
+
+    await sharp(file)
+      .webp()
+      .toFile(destination + '-large.webp');
   }))
 });
