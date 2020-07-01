3fd0234df32e199affdd6dd35cd5274168f4769e
Tom Hooijenga <tomhooijenga@gmail.com>
2020-07-01 17:33:47 +0200

    Add squashed images

---
+++ resize.js
@@ -0,0 +1,18 @@
+const glob = require('glob');
+const sharp = require('sharp');
+const mkdirp = require('mkdirp');
+const path = require('path');
+
+glob('./source/**/*.jpg', async (error, files) => {
+  await Promise.all(files.map( async (file) => {
+
+    const destination = file.replace('./source', './img').replace('.jpg', '.webp');
+    console.log(file, destination);
+
+    await mkdirp(path.dirname(destination));
+    await sharp(file)
+      .resize({width: 1000})
+      .webp()
+      .toFile(destination);
+  }))
+});
