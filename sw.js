5702ad848dd36eaf1f1c5af8efd46a4df5c49b9b
Tom Hooijenga <tomhooijenga@gmail.com>
2020-07-04 19:43:01 +0200

    Add service worker for offline access

---
+++ sw.js
@@ -0,0 +1,8 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js');
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+const {files, ver: revision} = self.__precacheManifest;
+workbox.precaching.precacheAndRoute(files.map((url) => ({url})));
+workbox.precaching.precacheAndRoute([{url: 'index.html', revision}]);
