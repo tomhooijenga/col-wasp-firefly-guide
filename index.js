51ca85607153848409fd3b3ddee91aa22032d137
Tom <tomhooijenga@gmail.com>
2022-05-31 22:00:03 +0200

    Load assets on demand

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register(new URL('./sw.js', import.meta.url), {type: 'module'});
+  const button = document.getElementById('make-offline');
+
+  navigator.serviceWorker
+    .register(new URL('./sw.js', import.meta.url), {type: 'module'})
+    .then(async () => {
+      const available = await sendMessage({type: 'CHECK_OFFLINE'})
+      if (available) {
+        markReady(button);
+      } else {
+        button.addEventListener('click', async () => {
+          markPending(button);
+          await sendMessage({type: 'MAKE_OFFLINE'});
+          markReady(button);
+        });
+      }
+    });
+}
+
+async function sendMessage(message) {
+  const messageChannel = new MessageChannel();
+
+  const {active: controller} = await navigator.serviceWorker.ready;
+
+  return new Promise((resolve) => {
+    messageChannel.port1.onmessage = (event) => {
+      resolve(event.data)
+    }
+
+    controller.postMessage(message, [messageChannel.port2]);
+  });
+}
+
+function markReady(button) {
+  button.innerText = 'Complete!';
+  button.dataset.offlineState = 'ready';
+}
+
+function markPending(button) {
+  button.innerText = 'Loading...';
+  button.dataset.offlineState = 'loading';
 }
--- sw.js
+++ sw.js
@@ -1,9 +1,39 @@
-import {skipWaiting, clientsClaim} from "workbox-core";
+import {cacheNames, clientsClaim} from 'workbox-core';
 import {precacheAndRoute} from 'workbox-precaching';
-import {manifest, version as revision } from '@parcel/service-worker';
+import {manifest, version as revision} from '@parcel/service-worker';
+import {registerRoute, Route} from 'workbox-routing';
+import {StaleWhileRevalidate} from 'workbox-strategies';
 
-skipWaiting();
+self.skipWaiting();
 clientsClaim();
 
-precacheAndRoute(manifest.map((url) => ({url, revision})));
 precacheAndRoute([{url: 'index.html', revision}]);
+precacheAndRoute(
+  manifest
+    .filter((url) => url.endsWith('.css'))
+    .map((url) => ({url, revision}))
+)
+
+registerRoute(new Route(
+  ({url}) => manifest.includes(url.pathname),
+  new StaleWhileRevalidate({
+    cacheName: cacheNames.runtime
+  })
+));
+
+addEventListener('message', async (event) => {
+  if (event.data.type === 'MAKE_OFFLINE') {
+    const cache = await caches.open(cacheNames.runtime)
+    await cache.addAll(manifest)
+    console.log('done');
+    event.ports[0].postMessage(true);
+  }
+  else if (event.data.type === 'CHECK_OFFLINE') {
+    const cache = await caches.open(cacheNames.runtime)
+    const matches = await Promise.all(
+      manifest.map((url) => cache.match(url))
+    )
+
+    event.ports[0].postMessage(matches.every(Boolean));
+  }
+});
